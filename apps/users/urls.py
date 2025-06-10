44d8c836d428d1b381315b9895331a732e514fb5
kubatbaew <kubatbaew@gmail.com>
2025-06-10 08:03:36 +0600

    add profile suffix

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("", UserListAPIView.as_view(), name="users_list"),
     path("<int:pk>/", UserDestroyAPIView.as_view(), name="users_delete"),
     path("<int:pk>/transfer", UserTransferAPIView.as_view(), name="users_transfer"),
-    path("<str:telegram_id>/", GetMeAPIView.as_view(), name="get_me"),
+    path("profile/<str:telegram_id>/", GetMeAPIView.as_view(), name="get_me"),
     path("create/", UserCreateAPIView.as_view(), name="create_user"),
 ]
