8fbed25c4771ccf26d40e1f35a66c4c34582f4a3
kubatbaew <kubatbaew@gmail.com>
2025-08-20 13:05:48 +0600

    add order_by

--- apps/clients/views.py
+++ apps/clients/views.py
@@ -4,7 +4,16 @@ from rest_framework.viewsets import ModelViewSet
 from apps.clients.models import Client
 from apps.clients.serializers import ClientListSerializer
 
+from django.db.models import IntegerField
+from django.db.models.functions import Cast, Substr
+
 
 class ClientListAPIViewSet(ModelViewSet):
-    queryset = Client.objects.all()
+    queryset = (
+        Client.objects
+        .annotate(
+            code_num=Cast(Substr('client_code', 4), IntegerField())
+        )
+        .order_by('code_num')
+    )
     serializer_class = ClientListSerializer
